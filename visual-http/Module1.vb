95bf43053149259e5010335d02e228de37c126a5
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-07-17 16:56:36 -0400

    Update Module1.vb

--- visual-http/Module1.vb
+++ visual-http/Module1.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Net
+Imports System.Net
 Imports System.IO
 Imports System.Text
 Imports System.Diagnostics
@@ -6,10 +6,26 @@ Imports System.Diagnostics
 Module Module1
 
     Sub Main()
+        Console.WriteLine("Loading Config File....")
+        Dim port As String = "8080"
+        Dim errorcontent As String = "<html><body><h1>404 - File Not Found</h1><br><p>powered by visual http</p></body></html>"
+        If File.Exists("config.yml") Then
+            Dim configLines() As String = File.ReadAllLines("config.yml")
+            For Each line As String In configLines
+                If line.StartsWith("port=") Then
+                    port = line.Substring(5)
+                End If
+                If line.StartsWith("error=") Then
+                    errorcontent = line.Substring(6)
+                End If
+            Next
+        End If
+        Console.WriteLine("Port As " + port)
+        Console.WriteLine("Error As " + errorcontent)
         Dim listener As New HttpListener()
-        listener.Prefixes.Add("http://localhost:8080/")
+        listener.Prefixes.Add("http://localhost:" + port + "/")
         listener.Start()
-        Console.WriteLine("Visual HTTP started on port 8080. Create an index.html file stored in the same directory, to load a page")
+        Console.WriteLine("Listening on Port " + port)
 
         While True
             Dim context As HttpListenerContext = listener.GetContext()
@@ -29,14 +45,13 @@ Module Module1
                 Dim output As Stream = response.OutputStream
                 output.Write(buffer, 0, buffer.Length)
                 output.Close()
+                Console.WriteLine("Connection recieved, file " + filePath + " displayed.")
             Else
-                Dim errorMessage As String = "<html><body><h1>404 - File Not Found</h1><br><p>powered by visual http</p></body></html>"
-                Dim buffer() As Byte = Encoding.UTF8.GetBytes(errorMessage)
+                Dim buffer() As Byte = Encoding.UTF8.GetBytes(errorcontent)
                 response.ContentLength64 = buffer.Length
                 Dim output As Stream = response.OutputStream
                 output.Write(buffer, 0, buffer.Length)
                 output.Close()
-
             End If
         End While
     End Sub
